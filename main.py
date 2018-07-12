13045e700f541fef6630966408251a2c87ec0bb1
Orsolya Lévai <solya0082@gmail.com>
2018-07-12 16:18:54 +0200

    Changed naming convention for Python

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import *
-from repository import userRepository
+from repository import user_repository
 import psycopg2
 
 app = Flask(__name__)
@@ -7,17 +7,17 @@ app.secret_key = "kutyafule"
 
 
 @app.route("/registration", methods=['POST'])
-def registerUser():
-    userdict = request.get_json(force=True)
-    firstname = userdict["firstName"]
-    lastname = userdict["lastName"]
-    email = userdict["email"]
-    password = userdict["password"]
-    address = userdict["address"]
-    phonenumber = userdict["phoneNum"]
+def register_user():
+    user_dict = request.get_json(force=True)
+    first_name = user_dict["firstName"]
+    last_name = user_dict["lastName"]
+    email = user_dict["email"]
+    password = user_dict["password"]
+    address = user_dict["address"]
+    phone_number = user_dict["phoneNum"]
 
     try:
-        userRepository.insertUser(firstname, lastname, email, password, address, phonenumber)
+        user_repository.insert_user(first_name, last_name, email, password, address, phone_number)
     except psycopg2.IntegrityError:
         return "duplicateEmail"
     return "Success"
@@ -27,7 +27,7 @@ def registerUser():
 def loginUser():
     emailDict = request.get_json(force=True)
     email = emailDict["email"]
-    user = userRepository.getUserByEmail(email)
+    user = user_repository.get_user_by_email(email)
     if user == None:
         return "userNotFound"
     return jsonify(user)
@@ -35,7 +35,7 @@ def loginUser():
 
 @app.route("/user/<int:id>", methods=['GET'])
 def getUser(id):
-    user = userRepository.getUserById(id)
+    user = user_repository.get_user_by_id(id)
     if user == None:
         return "userNotFound"
     return jsonify(user)
--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,8 +1,8 @@
-from repository import dataManager
+from repository import data_manager
 
 
-@dataManager.connection_handler
-def insertUser(cursor, firstname, lastname, email, password, address, phonenumber):
+@data_manager.connection_handler
+def insert_user(cursor, firstname, lastname, email, password, address, phonenumber):
     cursor.execute("""
                                         INSERT INTO shitwishusers (first_name, last_name, email, password, address, phone_number)
                                         VALUES (
@@ -19,8 +19,8 @@ def insertUser(cursor, firstname, lastname, email, password, address, phonenumbe
                                                   'address': address,
                                                   'phoneNumber': phonenumber})
 
-@dataManager.connection_handler
-def getUserByEmail(cursor, email):
+@data_manager.connection_handler
+def get_user_by_email(cursor, email):
     cursor.execute("""
                         SELECT * FROM shitwishusers
                         where email = %(email)s
@@ -28,8 +28,8 @@ def getUserByEmail(cursor, email):
     user = cursor.fetchone()
     return user
 
-@dataManager.connection_handler
-def getUserById(cursor, id):
+@data_manager.connection_handler
+def get_user_by_id(cursor, id):
     cursor.execute("""
                         SELECT * FROM shitwishusers
                         where id = %(id)s
